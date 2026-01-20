f748e271218f9ceb05ce2a805a8969c547da1fcd
RayyanDev <rayyan.kheisar@gloriglobalgener.com>
2026-01-20 10:31:40 +0700

    many table feat

---
+++ src/api/order-menu/controllers/order-menu.ts
@@ -0,0 +1,7 @@
+/**
+ * order-menu controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::order-menu.order-menu');
---
+++ src/api/order-menu/routes/order-menu.ts
@@ -0,0 +1,7 @@
+/**
+ * order-menu router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::order-menu.order-menu');
